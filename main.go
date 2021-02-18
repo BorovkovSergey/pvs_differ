521642af21548fb90a539992e7ab16e989f2776c
Borovkov <Sergey.Borovkov@infotecs.ru>
2021-02-18 11:38:44 +0700

    addded PrintDiff function

--- main.go
+++ main.go
@@ -5,6 +5,17 @@ import (
 	"os"
 )
 
+func PrintDiff(diff Alertions) {
+	count := 0
+	for i, v := range diff {
+		fmt.Printf("%d: %s\n", i, v.fileName)
+		for j, s := range v.text {
+			count++
+			fmt.Printf("\t\t%d: string[%s] %s\n", count, v.strNumbers[j], s)
+		}
+	}
+}
+
 func main() {
 	args := os.Args
 	if len(args) < 3 {
@@ -18,12 +29,5 @@ func main() {
 	newAlertions := parse(newFile)
 
 	diffAlertions := oldAlertions.MakeAlertionsDiff(newAlertions)
-	count := 0
-	for i, v := range diffAlertions {
-		fmt.Printf("%d: %s\n", i, v.fileName)
-		for j, s := range v.text {
-			count++
-			fmt.Printf("\t\t%d: string[%s] %s\n", count, v.strNumbers[j], s)
-		}
-	}
+	PrintDiff(diffAlertions)
 }
